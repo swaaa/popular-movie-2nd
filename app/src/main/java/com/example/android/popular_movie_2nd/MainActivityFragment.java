aa581f23225663bc7d8e0492c894762e7632eb08
swaaa <mike@von-dem-brinke.de>
2015-07-28 20:38:18 +0200

    add Dummy Data

--- app/src/main/java/com/example/android/popular_movie_2nd/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popular_movie_2nd/MainActivityFragment.java
@@ -32,13 +32,9 @@ public class MainActivityFragment extends Fragment {
     };
     List<Integer> thumbIds = new ArrayList<>(Arrays.asList(imageGrid));
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
