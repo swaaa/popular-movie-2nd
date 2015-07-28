be0913b5ad5cd745ce3aaa8782ed73700b797200
swaaa <mike@von-dem-brinke.de>
2015-07-28 19:30:55 +0200

    add Dummy Data

--- app/src/main/java/com/example/android/popular_movie_2nd/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popular_movie_2nd/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,13 +17,6 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     /**
@@ -33,4 +30,15 @@ public class MainActivityFragment extends Fragment {
             R.mipmap.ic_launcher,
             R.mipmap.ic_launcher
     };
+    List<Integer> thumbIds = new ArrayList<>(Arrays.asList(imageGrid));
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
 }
